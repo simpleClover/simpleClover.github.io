f933a1297efe66e7ff8fd55dffbd8a549bee8a1e
SimpleClover <3138947771@qq.com>
2025-03-31 23:12:31 +0800

    Site updated: 2025-03-31 23:12:31

--- js/main.js
+++
@@ -1,6 +0,0 @@
-var navlink = document.querySelector('#nav-link');
-document.addEventListener("keydown", function (event) {
-    if (event.altKey && event.key.toUpperCase() === "K") {
-        navlink.click();
-    }
-});
